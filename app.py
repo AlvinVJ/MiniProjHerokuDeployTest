ca1590c8be30d1bc06e96442bb9e88ee86eb1587
AlvinVJ <163581592+AlvinVJ@users.noreply.github.com>
2025-01-15 22:04:14 +0530

    hi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Hello, World!"
+    return "Hello, World! this is a test"
 
 if __name__ == '__main__':
     app.run()
