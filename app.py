387d7d06b15c915a23252d3600d8c96ae4b9b5eb
AlvinVJ <163581592+AlvinVJ@users.noreply.github.com>
2025-01-15 22:13:06 +0530

    trial

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, Response, request
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello, World! this is a test"
+video_frame = None  # Global variable to hold the latest video frame
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/upload', methods=['POST'])
+def upload_frame():
+    global video_frame
+    video_frame = request.data  # Get the video frame sent by the sender
+    return "Frame received", 200
+
+
+@app.route('/stream')
+def stream_video():
+    def generate():
+        global video_frame
+        while True:
+            if video_frame:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + video_frame + b'\r\n')
+    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
